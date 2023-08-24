4a0c3e3bfc6c602f53c12d0d65e4583fef187a4b
Jake Felice <jasonfelice0@gmail.com>
2023-08-24 04:48:56 -0700

    Create fetch function and render cards from data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from '../src/components/Header.js';
+import Card from '../src/components/Card.js';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const jData = await axios.get('https://www.reddit.com/r/reactjs.json');
+    setData(jData.data.data.children);
+  }
+  
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <Header />
+      <section className="cards">
+       {
+        data.map((child) => (
+          <Card data={{ title: child.title, url: child.url, score: child.score, selftext: child.selftext_html}} />
+        ))
+       }
+      </section>
     </div>
   );
 }
